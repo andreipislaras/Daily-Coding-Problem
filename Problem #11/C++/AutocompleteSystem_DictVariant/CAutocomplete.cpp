fac3272cf7093c0a50d0efff6cea95ffe1acf321
Pislaras <uidv7600@contiwan.com>
2019-04-06 16:47:00 +0300

    problem 11

--- Problem #11/C++/AutocompleteSystem_DictVariant/CAutocomplete.cpp
+++ Problem #11/C++/AutocompleteSystem_DictVariant/CAutocomplete.cpp
@@ -18,6 +18,19 @@ bool CAutocomplete::isStringInMap(string str) {
     return  retVal;
 }
 
+/*
+ * str = deer
+ * mWordsMap[d] = deer
+ * mWordsMap[de] = deer
+ * mWordsMap[dee] = deer
+ * mWordsMap[deer] = deer
+ *
+ * str = deal
+ * mWordsMap[d] = deer, deal
+ * mWordsMap[de] = deer, deal
+ * mWordsMap[dea] = deal
+ * mWordsMap[deal] = deal
+ * */
 void CAutocomplete::addWord(string str) {
     for (int i = 0; i < str.length(); ++i) {
         string temp = str.substr(0, i+1);
